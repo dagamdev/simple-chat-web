4e7b4f8610225337aa506096cfa34e519a3c405c
Dagadev <gacub20ru@gmail.com>
2023-09-19 19:33:19 -0600

    update style

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 export default function SignupPage () {
   return (
-    <main className='min-h-screen flex justify-center items-center flex-col'>
+    <main className='min-h-screen flex justify-center items-center bg-zinc-200'>
       <AuthForm type='signup' />
     </main>
   )
