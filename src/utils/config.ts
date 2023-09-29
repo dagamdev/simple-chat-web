746c4efc22d70a9765246ca1baa60fcfce81238d
Dagadev <gacub20ru@gmail.com>
2023-09-29 12:49:36 -0600

    update ws to wss

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 // export const API_HOST = 'localhost:8000'
 export const API_HOST = 'simple-chat-api-dagamdev.koyeb.app'
 export const API_ENDPOINT = `https://${API_HOST}/api/v2/`
-export const API_WS_ENDPOINT = `ws://${API_HOST}/ws/`
+export const API_WS_ENDPOINT = `wss://${API_HOST}/ws/`
 export const MAIN_COLOR = '#4A87C9'
 export const PROJECT_NAME = 'Simple chat'
