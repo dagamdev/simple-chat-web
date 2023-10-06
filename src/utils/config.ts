47748e46db5b59053748691562f8d6ac12cea096
Dagadev <gacub20ru@gmail.com>
2023-10-05 20:06:42 -0600

    add version variable

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,3 +4,4 @@ export const API_ENDPOINT = `https://${API_HOST}/api/v2/`
 export const API_WS_ENDPOINT = `wss://${API_HOST}/ws/`
 export const MAIN_COLOR = '#4A87C9'
 export const PROJECT_NAME = 'Simple chat'
+export const VERSION = '1.5.2'
