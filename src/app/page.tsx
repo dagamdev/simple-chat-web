a25bc90f4c9a624cc4bf5561e1eec0bb7e723996
Dagadev <gacub20ru@gmail.com>
2023-10-06 21:51:10 -0600

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,11 @@ export default function Home () {
   return (
     <main className="flex min-h-screen items-center justify-center p-6">
       <section className='flex flex-col max-w-600'>
-        <h1 className={'font-bold text-2xl text-[#4A87C9]'}>Bienvenido</h1>
-        <p className='mb-4'>Inicia seccion o registrate para empesar a chatear</p>
+        <h1 className={'font-bold text-2xl text-[#4A87C9]'}>Bienvenido{user === undefined ? '' : ' de nuevo'}</h1>
+        <p className='mb-4'>{user === undefined
+          ? 'Inicia seccion o registrate para empesar a chatear'
+          : 'Te estamos Redirigiendo a tus conversaciones...'
+        }</p>
         {user === undefined && <>
             <Link href={'/login'} className='button font-bold px-10 py-2 rounded-lg border-2 border-[#4A87C9] my-2 shadow-md text-center text-gray-900 bg-[#4A87C9]'>Iniciar sesión</Link>
             <Link href={'/signup'} className='button font-bold px-10 py-2 rounded-lg border-2 border-[#4A87C9] my-2 shadow-md text-center text-[#4A87C9]'>Resgistrarme</Link>
